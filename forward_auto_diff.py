058259493e79d8438a7e3f16cc1619bc07b62fb1
Christopher-Berglund <berglund.christopher1@gmail.com>
2024-03-16 14:10:51 -0700

    Added operator overloading to Var class
    Implemented operator overloading for add, mul, and pow. Expression in function has these operators now, except for + because the program breaks without the sum function

--- forward_auto_diff.py
+++ forward_auto_diff.py
@@ -7,12 +7,45 @@ class Var:
     def __init__(self, val, der):
         self.val = val
         self.der = der
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            return Var(self.val+other.val, self.der+other.der)
+        else:
+            return Var(self.val+other, self.der)
+    def __mul__(self, other):
+        if isinstance(other, self.__class__):
+            return Var(self.val*other.val, self.der*other.val+self.val*other.der)
+        else:
+            return Var(self.val*other, self.der*other)
+    def __pow__(self, other):
+        if isinstance(other, self.__class__):
+            new_val = self.val ** other.val
+            new_der = new_val * (other.der * math.log(self.val) + other.val * self.der/self.val)
+            return Var(new_val, new_der)
+        else:
+            new_val = self.val ** other
+            new_der = self.der * other * (self.val ** (other - 1))
+            return Var(new_val, new_der)
+    def __rpow__(self,other):
+        if isinstance(other, self.__class__):
+            new_val = other.val ** self.val
+            new_der = new_val * (self.der * math.log(other.val) + self.val * other.der/other.val)
+            return Var(new_val, new_der)
+        else:
+            new_val = other ** self.val
+            new_der = new_val * math.log(other) * self.der
+            return Var(new_val, new_der)
+        
+    __radd__ = __add__
+    __rmul__ = __mul__
+        
+            
 
 def neg(var):
     return Var(var.val, -var.der)
 
-def c(k, var):
-    return Var(var.val, k*var.der)
+#def c(k, var):
+#    return Var(var.val, k*var.der)
 
 def sum(vars):
     deriv = 0
@@ -22,18 +55,18 @@ def sum(vars):
         value += i.val
     return Var(value, deriv)
 
-def product(vars):
-    deriv = 0
-    if len(vars) == 0:
-        return Var(1, 0)
-    value = math.prod(var.val for var in vars)
-    for i in range(len(vars)):
-        deriv_part = vars[i].der
-        for j in range(len(vars)):
-            if i != j:
-                deriv_part *= vars[j].val
-        deriv += deriv_part
-    return Var(value, deriv)
+#def product(vars):
+#    deriv = 0
+#    if len(vars) == 0:
+#        return Var(1, 0)
+#    value = math.prod(var.val for var in vars)
+#    for i in range(len(vars)):
+#        deriv_part = vars[i].der
+#        for j in range(len(vars)):
+#            if i != j:
+#                deriv_part *= vars[j].val
+#        deriv += deriv_part
+#    return Var(value, deriv)
 
 def sin(var):
     if isinstance(var, tuple):
@@ -69,24 +102,26 @@ def cos(var):
     else:
         return Var(math.cos(var.val), -var.der * math.sin(var.val))
 
-def pow(n, var):
-    if isinstance(var, tuple):
-        deriv = 0
-        val = 1
-        for i in range(len(var)):
-            inner_value = var[i].val
-            for j in range(len(var)):
-                if i != j:
-                    inner_value *= var[j].val
-            deriv += var[i].der * n * math.pow(inner_value, n - 1)
-            val *= math.pow(var[j].val, n)
-        return Var(val, deriv)
-    else:
-        return Var(math.pow(var.val, n), var.der * n * math.pow(var.val, n - 1))
+#def pow(n, var):
+#    if isinstance(var, tuple):
+#        deriv = 0
+#        val = 1
+#        for i in range(len(var)):
+#            inner_value = var[i].val
+#            for j in range(len(var)):
+#                if i != j:
+#                    inner_value *= var[j].val
+#            deriv += var[i].der * n * math.pow(inner_value, n - 1)
+#            val *= math.pow(var[j].val, n)
+#        return Var(val, deriv)
+#    else:
+#        return Var(math.pow(var.val, n), var.der * n * math.pow(var.val, n - 1))
 
 
 def funct(x,y):
-    f1 = sum((pow(2,y), c(2,neg(product((y, pow(2,x))))), pow(4,x), neg(c(2,x)), pow(2,x)))
+    #f1 = (y+neg(x**2))**2 + (1+ neg(x))**2
+    #f1 = sum((pow(2,y), c(2,neg(product((y, pow(2,x))))), pow(4,x), neg(c(2,x)), pow(2,x)))
+    f1 = sum((y**2, 2*neg(y * x**2), x**4, neg(2*x), x**2))
     return f1 
 
 def compute_grad(vars):
@@ -97,24 +132,29 @@ def compute_grad(vars):
         vars[i].der = 0
     return grad
 
-# def main():
-#     x = Var(3, 1)
-#     y = Var(5, 0)
-#     z = Var(2, 0)
-#     f = funct(x,y)
-#     print(f.der)
+def main():
+     x = Var(3, 1)
+     y = Var(5, 0)
+     z = Var(2, 0)
+     
+     u = x+y
+     print(u)
+     
+     f = funct(x,y)
+     print(f.der)
     
-#     x.der = 0
-#     y.der = 1
-#     f = funct(x,y)
-#     print(f.der)
+     x.der = 0
+     y.der = 1
+     f = funct(x,y)
+     print(f.der)
     
-#     x = Var(3, 1)
-#     y = Var(5, 0)
-#     z = Var(2, 0)
-#     vars = (x, y)
-#     grad = compute_grad(vars)
-#     print(grad)
+     x = Var(3, 1)
+     y = Var(5, 0)
+     z = Var(2, 0)
+     vars = (x, y)
+     grad = compute_grad(vars)
+     print(grad)
 
 
-# main()
+#main()
+
