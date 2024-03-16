e075c2a63443a3f61c59bfffc6cb73eda8daa853
Pierre Visconti <94921305+petrosvisconte@users.noreply.github.com>
2024-03-16 10:47:57 -0700

    Working auto_diff

--- forward_auto_diff.py
+++ forward_auto_diff.py
@@ -4,86 +4,90 @@ import math
 
 
 class Var:
-    def __init__(self, val, der, seed):
+    def __init__(self, val, der):
         self.val = val
         self.der = der
-        self.seed = seed
 
 def neg(var):
-    return Var(var.val, -var.der, var.seed)
+    return Var(var.val, -var.der)
 
 def c(k, var):
-    return Var(var.val, k*var.der, var.seed)
+    return Var(var.val, k*var.der)
 
 def sum(vars):
     deriv = 0
+    value = 0
     for i in vars:
         deriv += i.der
-    return Var(0, deriv)
+        value += i.val
+    return Var(value, deriv)
 
 def product(vars):
     deriv = 0
+    if len(vars) == 0:
+        return Var(1, 0)
+    value = math.prod(var.val for var in vars)
     for i in range(len(vars)):
         deriv_part = vars[i].der
         for j in range(len(vars)):
             if i != j:
                 deriv_part *= vars[j].val
         deriv += deriv_part
-    return Var(0, deriv)
+    return Var(value, deriv)
 
-def sin(vars):
-    try:
-        iter(vars)
-        if isinstance(vars, tuple):
-            deriv = 0
-            for i in range(len(vars)):
-                deriv_part = vars[i].der
-                inner_value = vars[i].val
-                for j in range(len(vars)):
-                    if i != j:
-                        deriv_part *= vars[j].val
-                        inner_value *= vars[j].val
-                deriv += deriv_part*math.cos(inner_value)
-            return Var(0, deriv)
-    except TypeError:
-        return Var(0, vars.der*math.cos(vars.val))
-    
-def cos(vars):
-    try:
-        iter(vars)
-        if isinstance(vars, tuple):
-            deriv = 0
-            for i in range(len(vars)):
-                deriv_part = vars[i].der
-                inner_value = vars[i].val
-                for j in range(len(vars)):
-                    if i != j:
-                        deriv_part *= vars[j].val
-                        inner_value *= vars[j].val
-                deriv += deriv_part*math.sin(inner_value)
-            return Var(0, -deriv)
-    except TypeError:
-        return Var(0, -vars.der*math.sin(vars.val))
+def sin(var):
+    if isinstance(var, tuple):
+        deriv = 0
+        val = 1
+        for i in range(len(var)):
+            deriv_part = var[i].der
+            inner_value = var[i].val
+            for j in range(len(var)):
+                if i != j:
+                    deriv_part *= var[j].val
+                    inner_value *= var[j].val
+            deriv += deriv_part * math.cos(inner_value)
+            val *= math.sin(var[j].val)
+        return Var(val, deriv)
+    else:
+        return Var(math.sin(var.val), var.der * math.cos(var.val))
 
-def pow(n, vars):
-    try:
-        iter(vars)
-        if isinstance(vars, tuple):
-            deriv = 0
-            for i in range(len(vars)):
-                inner_value = vars[i].val
-                for j in range(len(vars)):
-                    if i != j:
-                        inner_value *= vars[j].val
-                deriv += vars[i].der*n*math.pow(inner_value, n-1)
-            return Var(0, deriv)
-    except TypeError:
-        return Var(0, vars.der*n*math.pow(vars.val, n-1))
+def cos(var):
+    if isinstance(var, tuple):
+        deriv = 0
+        val = 1
+        for i in range(len(var)):
+            deriv_part = var[i].der
+            inner_value = var[i].val
+            for j in range(len(var)):
+                if i != j:
+                    deriv_part *= var[j].val
+                    inner_value *= var[j].val
+            deriv -= deriv_part * math.sin(inner_value)
+            val *= math.cos(var[j].val)
+        return Var(val, -deriv)
+    else:
+        return Var(math.cos(var.val), -var.der * math.sin(var.val))
+
+def pow(n, var):
+    if isinstance(var, tuple):
+        deriv = 0
+        val = 1
+        for i in range(len(var)):
+            inner_value = var[i].val
+            for j in range(len(var)):
+                if i != j:
+                    inner_value *= var[j].val
+            deriv += var[i].der * n * math.pow(inner_value, n - 1)
+            val *= math.pow(var[j].val, n)
+        return Var(val, deriv)
+    else:
+        return Var(math.pow(var.val, n), var.der * n * math.pow(var.val, n - 1))
+
 
 def funct(x,y):
-    w1 = product((sum((y, neg(product((x,x))))), sum((y, neg(product((x,x)))))))
-    w2 = sum((w1, neg(c(2,x)), product((x,x))))
-    return w2
+    f1 = sum((pow(2,y), c(2,neg(product((y, pow(2,x))))), pow(4,x), neg(c(2,x)), pow(2,x)))
+    return f1 
 
 def compute_grad(vars):
     grad = [0] * len(vars) 
@@ -93,23 +97,24 @@ def compute_grad(vars):
         vars[i].der = 0
     return grad
 
-def main():
-    x = Var(3, 1)
-    y = Var(5, 0)
-    z = Var(2, 0)
-    f = funct(x,y)
-    print(f.der)
+# def main():
+#     x = Var(3, 1)
+#     y = Var(5, 0)
+#     z = Var(2, 0)
+#     f = funct(x,y)
+#     print(f.der)
     
-    x.der = 0
-    y.der = 1
-    f = funct(x,y)
-    print(f.der)
+#     x.der = 0
+#     y.der = 1
+#     f = funct(x,y)
+#     print(f.der)
     
-    vars = (x, y)
-    grad = compute_grad(vars)
-    print(grad)
+#     x = Var(3, 1)
+#     y = Var(5, 0)
+#     z = Var(2, 0)
+#     vars = (x, y)
+#     grad = compute_grad(vars)
+#     print(grad)
 
-    x.der = 1
-    print(sin(((x,y,z))).der)
 
-main()
+# main()
